38b60bff3ad36412ec32d2c2daab22432b6ef190
Goly67 <tufysooy@gmail.com>
2024-10-25 11:41:36 +0800

    AboutError.js

--- scripts/AboutError.js
+++ scripts/AboutError.js
@@ -1,5 +1,4 @@
 document.getElementById('backToNews').addEventListener('click', function(e) {
     e.preventDefault();
-    alert('Returning to the news page...');
     window.location.href = 'https://goly67.github.io/OctaNews/';
 });
